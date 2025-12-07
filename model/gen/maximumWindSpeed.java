731f93355e0c6f4928903b0f48a9351f4af3dbb0
kusala9 <kusala9@googlemail.com>
2025-12-07 13:45:53 +0000

    add back xml stubs

---
+++ model/gen/maximumWindSpeed.java
@@ -0,0 +1,20 @@
+
+
+/**
+ * @author kusal
+ * @version 1.0
+ * @created 30-Nov-2025 14:11:16
+ */
+public class maximumWindSpeed {
+
+	private int information;
+
+	public maximumWindSpeed(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+}
